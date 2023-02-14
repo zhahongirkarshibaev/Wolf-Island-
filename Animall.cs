7d19380772977da8910ce75ae554e1ad7193e60d
Zhahongir <96703268+zhahongirkarshibaev@users.noreply.github.com>
2023-02-14 11:00:03 +0300

    Add files via upload

---
+++ Animall.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolfIsland
+{
+    internal abstract class Animall
+    {
+        private Random random = new Random();
+        public const int MaxFiald = 20;
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        abstract public Animall Reprodaction();
+        protected int[,] Map = new int[,] { { -1, -1 }, { 0, -1 }, { 1, -1 }, { -1, 0 }, { 0, 1 }, {1, 0}, { -1, 1 }, { 1, 1 } };
+        public void Muve(int m)
+        {
+            if (m != 8)
+            {
+                X = (Map[m, 0] + X >= 0 &&
+                Map[m, 0] + X < MaxFiald) ? X + Map[m, 0] : X;
+                Y =  (Map[m, 1] + Y >= 0 &&
+                Map[m, 1] + Y < MaxFiald) ? Y + Map[m, 1] : Y;
+            }
+        }
+        public Animall(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+
+        }
+        public Animall()
+        {
+            X = random.Next(MaxFiald -1);
+            Y = random.Next(MaxFiald -1);
+        }
+    }
+}
---
+++ Rebite.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WolfIsland
+{
+    internal class Rebite : Animall
+    {
+        private Random random = new Random();
+        public Rebite()
+        {
+
+        }
+        public void Muve()
+        {
+            int m = random.Next(0, 7);
+            base.Muve(m);
+        }
+        override
+        public Animall Reprodaction()
+        {
+            int h = random.Next(1, 5);
+            if (h == 3)
+            {
+                return new Rebite();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
